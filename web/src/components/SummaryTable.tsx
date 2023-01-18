45ce104b1aaafa92cc1d89cf5999e622fb5852ec
vepifanio <vepifanio1@gmail.com>
2023-01-18 12:03:26 -0300

    feat: Fulfill Summary Table Elements added

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,11 @@
+import { useId } from "react";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const summaryDates = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -17,11 +22,16 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map(() => (
+            <div
+              key={useId()}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
       </div>
     </div>
   );
