c824c200b48c74af976d8211affc6c4d8fa02a8e
vepifanio <vepifanio1@gmail.com>
2023-01-18 17:16:55 -0300

    feat: Habit Day created and show them in Home screen

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummarDatesSize = 18 * 5;
+const amountOfDaysToFill = minimumSummarDatesSize - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -23,7 +27,26 @@ export function Home() {
         ))}
       </View>
 
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{
+                  width: DAY_SIZE,
+                  height: DAY_SIZE,
+                }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
